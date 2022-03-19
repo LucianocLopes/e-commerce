6d18f12b62b9395c38fcbd8ac6efc5b7cf7dba5b
LucianocLopes <luciano_clopes@hotmail.com>
2022-03-18 23:12:23 -0300

    config app admin products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class VariationInLine(admin.TabularInline):
+    model = models.Variation
+    extra = 1
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        VariationInLine
+    ]
+
+
+admin.site.register(models.Variation)
